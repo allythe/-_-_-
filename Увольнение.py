9bc0b7b95e6595a24ae13bb6cd8393a98912d6d9
allythe <cvrurgal@yandex.ru>
2023-02-27 13:52:33 +0300

    upd

--- Увольнение.py
+++ Увольнение.py
@@ -1,4 +1,5 @@
 import numpy as np
+import copy
 
 
 def make_graph(h, s):
@@ -11,27 +12,45 @@ def make_graph(h, s):
     return children, parent
 
 
-s = [1, 3, 4, 5, 2]  # номера работников
-h = [2, 5, 3, 0, 5]  # начальники работников
-remain = [3, 5]  # какие работники должны остаться
-children, parent = make_graph(h, s)
+def main():
+    with open('dismissal.txt') as file:
+        # кол-во сотрудников и запросов
+        n, q = list(map(int, file.readline().split()))
+        s, h = [], []
+        for i in range(n):
+            cur_s, cur_h = list(map(int, file.readline().split()))
+            s.append(cur_s)  # номера работников
+            h.append(cur_h)  # начальники работников
 
-to_delete = list(np.unique(s+h))
-for el in remain:
-    to_delete.remove(el)
-to_delete.remove(0)
+        children_graph, parent_graph = make_graph(h, s)
 
-for el in to_delete:
-    par = parent[el]
+        for i in range(q):
+            children, parent = copy.deepcopy(
+                children_graph), copy.deepcopy(parent_graph)
 
-    if el in children:
-        for child in children[el]:
-            parent[child] = par
-            children[par].append(child)
+            k = int(file.readline())  # кол-во оставшихся сотрудников
+            remain = list(map(int, file.readline().split()))
 
-        children.pop(el)
-    parent.pop(el)
+            to_delete = list(np.unique(s+h))
+            for el in remain:
+                to_delete.remove(el)
+            to_delete.remove(0)
 
-    children[par].remove(el)
+            for el in to_delete:
+                par = parent[el]
 
-print(parent)
+                if el in children:
+                    for child in children[el]:
+                        parent[child] = par
+                        children[par].append(child)
+
+                    children.pop(el)
+                parent.pop(el)
+                children[par].remove(el)
+
+            for k in parent.keys():
+                print(k, parent[k])
+
+
+if __name__ == '__main__':
+    main()
